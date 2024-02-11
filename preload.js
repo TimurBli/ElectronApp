baa9f9913d7186dc082e073c713dafe9bb0c7368
Timur_Cold <90599499+TimurBli@users.noreply.github.com>
2024-02-11 21:43:18 +0100

    Add files via upload

---
+++ preload.js
@@ -0,0 +1,23 @@
+// Toutes les API Node.js sont disponibles dans le processus de préchargement.
+// Il a la même sandbox qu'une extension Chrome.
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
+
+
+class printH {
+  login (){
+    console.log('tageule hugo')
+  }  
+}
+
+const test = () =>{
+  printH.login();
+}
